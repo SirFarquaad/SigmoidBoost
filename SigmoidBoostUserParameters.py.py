ff301386439df1223445da8c319b9b6a4ee3a901
SirFarquaad <97997546+SirFarquaad@users.noreply.github.com>
2022-01-30 14:19:19 -0500

    Create SigmoidBoostUserParameters.py.py

---
+++ SigmoidBoostUserParameters.py.py
@@ -0,0 +1,9 @@
+# User Paramaters
+# ---------------------------------------
+B = 0.20 # Max Boost Pctg
+MnD = 0 # Min No Days
+IL = 7 # No Linear Initial Days
+FL = 7 # No Linear Final Days
+MxD = 180 # Max No Days
+LS = -2 # Lower Sigmoid Slice
+US = 2 # Upper Sigmoid Slice
